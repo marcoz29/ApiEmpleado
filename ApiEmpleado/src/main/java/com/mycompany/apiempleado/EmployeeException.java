5d867f7e44015a1dce31490fbf294f889ea2aa66
marcoz29 <108309661+marcoz29@users.noreply.github.com>
2024-03-01 17:34:56 -0600

    Add files via upload

---
+++ ApiEmpleado/src/main/java/com/mycompany/apiempleado/EmployeeException.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.apiempleado;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Marco
+ */
+public interface EmployeeException {
+    
+    public void addEmployee(Employee employee);
+    
+    public Collection<Employee> getEmployees();
+    
+    public Employee editEmployee(Employee employee);
+    
+    public 
+            
+}
---
+++ ApiEmpleado/src/main/java/com/mycompany/apiempleado/EmployeeExeption.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apiempleado;
+
+/**
+ *
+ * @author Marco
+ */
+public class EmployeeExeption extends Exception{
+    
+    public EmployeeExeption(){
+        super();
+    }
+    
+    public EmployeeExeption(String message){
+        super(message);
+    }
+    
+}
